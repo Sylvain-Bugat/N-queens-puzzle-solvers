35d5656f7f032d1d3260947be9d361b1f57c0047
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-05-19 09:21:56 +0200

    Add test

---
+++ src/test/java/com/github/sbugat/nqueens/solvers/bruteforce/BruteForceNQueensSolverColumnBitFlagsTest.java
@@ -0,0 +1,20 @@
+package com.github.sbugat.nqueens.solvers.bruteforce;
+
+import com.github.sbugat.nqueens.GenericNQueensSolverTemplateTest;
+
+/**
+ * Brute-force N queens solver test.
+ * 
+ * @author Sylvain Bugat
+ * 
+ */
+public final class BruteForceNQueensSolverColumnBitFlagsTest extends GenericNQueensSolverTemplateTest {
+
+	/** Test only with a size of 6 because this algorithm is slow. */
+	private static final int MAXIMUM_TESTED_CHESSBOARD_SIZE = 7;
+
+	public BruteForceNQueensSolverColumnBitFlagsTest() {
+
+		super(new BruteForceNQueensSolverColumnBitFlags(MAXIMUM_TESTED_CHESSBOARD_SIZE, false), MAXIMUM_TESTED_CHESSBOARD_SIZE);
+	}
+}
