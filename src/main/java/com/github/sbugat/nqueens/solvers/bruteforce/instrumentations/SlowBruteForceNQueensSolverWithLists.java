3cf8786b422b114ce6da3e79b1f18a4b74495489
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-05-27 23:09:42 +0200

    Instrument the brute-force solver

--- src/main/java/com/github/sbugat/nqueens/solvers/bruteforce/instrumentations/SlowBruteForceNQueensSolverWithLists.java
+++ src/main/java/com/github/sbugat/nqueens/solvers/bruteforce/instrumentations/SlowBruteForceNQueensSolverWithLists.java
@@ -34,6 +34,7 @@ public final class SlowBruteForceNQueensSolverWithLists extends GenericInstrumen
 	public long solve() {
 
 		// Start the algorithm at the first position
+		methodCallsCount++;
 		solve(0, 0);
 
 		// Return the number of solutions found
@@ -49,15 +50,20 @@ public final class SlowBruteForceNQueensSolverWithLists extends GenericInstrumen
 	private void solve(final int x, final int y) {
 
 		// Put a queen on the current position
+		queenPlacementsCount++;
+		methodCallsCount += 2;
+		squareWritesCount++;
 		chessboard.get(x).set(y, Boolean.TRUE);
-		queenPlacementCount++;
 
 		// All queens are sets on the chessboard then a solution may be present
-		testsCount++;
+		explicitTestsCount++;
+		methodCallsCount++;
 		if (getPlacedQueens() >= chessboardSize) {
-			testsCount++;
+			explicitTestsCount++;
+			methodCallsCount++;
 			if (checkSolutionChessboard()) {
 				solutionCount++;
+				methodCallsCount++;
 				print();
 			}
 		}
@@ -66,36 +72,42 @@ public final class SlowBruteForceNQueensSolverWithLists extends GenericInstrumen
 			// Recursive call to the next position
 			final int nextX = (x + 1) % chessboardSize;
 			// Switch to the next line
-			testsCount++;
+			explicitTestsCount++;
 			if (0 == nextX) {
 
 				// End of the chessboard check
-				testsCount++;
+				explicitTestsCount++;
 				if (y + 1 < chessboardSize) {
+					methodCallsCount++;
 					solve(nextX, y + 1);
 				}
 			}
 			else {
+				methodCallsCount++;
 				solve(nextX, y);
 			}
 		}
 
 		// Remove the queen on the current position
+		methodCallsCount += 2;
+		squareWritesCount++;
 		chessboard.get(x).set(y, Boolean.FALSE);
 
 		// Recursive call to the next position
 		final int nextX = (x + 1) % chessboardSize;
 		// Switch to the next line
-		testsCount++;
+		explicitTestsCount++;
 		if (0 == nextX) {
 
 			// End of the chessboard check
-			testsCount++;
+			explicitTestsCount++;
 			if (y + 1 < chessboardSize) {
+				methodCallsCount++;
 				solve(nextX, y + 1);
 			}
 		}
 		else {
+			methodCallsCount++;
 			solve(nextX, y);
 		}
 	}
@@ -110,15 +122,23 @@ public final class SlowBruteForceNQueensSolverWithLists extends GenericInstrumen
 		int placedQueens = 0;
 
 		// Count all queens on the chessboard
+		implicitTestsCount++;
 		for (int x = 0; x < chessboardSize; x++) {
 
+			implicitTestsCount++;
 			for (int y = 0; y < chessboardSize; y++) {
 
-				testsCount++;
+				explicitTestsCount++;
+				methodCallsCount += 3;
+				squareReadsCount++;
 				if (chessboard.get(x).get(y).booleanValue()) {
 					placedQueens++;
 				}
+
+				implicitTestsCount++;
 			}
+
+			implicitTestsCount++;
 		}
 		return placedQueens;
 	}
@@ -131,65 +151,114 @@ public final class SlowBruteForceNQueensSolverWithLists extends GenericInstrumen
 	private boolean checkSolutionChessboard() {
 
 		// Check if 2 queens are on the same line
+		implicitTestsCount++;
 		for (int y = 0; y < chessboardSize; y++) {
 
 			boolean usedLine = false;
+			implicitTestsCount++;
 			for (int x = 0; x < chessboardSize; x++) {
 
-				testsCount++;
+				explicitTestsCount++;
+				methodCallsCount += 3;
+				squareReadsCount++;
 				if (chessboard.get(x).get(y).booleanValue()) {
+					explicitTestsCount++;
 					if (usedLine) {
 						return false;
 					}
 					usedLine = true;
 				}
+
+				implicitTestsCount++;
 			}
+			implicitTestsCount++;
+			implicitTestsCount++;
 		}
 
 		// Check if 2 queens are on the same column
+		implicitTestsCount++;
 		for (int x = 0; x < chessboardSize; x++) {
 
 			boolean usedColumn = false;
+			implicitTestsCount++;
 			for (int y = 0; y < chessboardSize; y++) {
 
-				testsCount++;
+				explicitTestsCount++;
+				methodCallsCount += 3;
+				squareReadsCount++;
 				if (chessboard.get(x).get(y).booleanValue()) {
+					explicitTestsCount++;
 					if (usedColumn) {
 						return false;
 					}
 					usedColumn = true;
 				}
+
+				implicitTestsCount++;
 			}
+
+			implicitTestsCount++;
 		}
 
 		// Check if 2 queens are on the same descending diagonal
+		implicitTestsCount++;
 		for (int diagonal = 0; diagonal < chessboardSize * 2 - 1; diagonal++) {
 			boolean usedDiagonal = false;
+			implicitTestsCount++;
 			for (int y = 0; y < chessboardSize; y++) {
 				final int x = diagonal - y;
-				testsCount++;
-				if (x >= 0 && x < chessboardSize && chessboard.get(x).get(y).booleanValue()) {
-					if (usedDiagonal) {
-						return false;
+				explicitTestsCount++;
+				if (x >= 0) {
+					explicitTestsCount++;
+					if (x < chessboardSize) {
+						explicitTestsCount++;
+						methodCallsCount += 3;
+						squareReadsCount++;
+						if (chessboard.get(x).get(y).booleanValue()) {
+							explicitTestsCount++;
+							if (usedDiagonal) {
+								return false;
+							}
+							usedDiagonal = true;
+						}
 					}
-					usedDiagonal = true;
 				}
+
+				implicitTestsCount++;
 			}
+
+			implicitTestsCount++;
 		}
 
 		// Check if 2 queens are on the same ascending diagonal
+		implicitTestsCount++;
 		for (int diagonal = 0; diagonal < chessboardSize * 2 - 1; diagonal++) {
 			boolean usedDiagonal = false;
+			implicitTestsCount++;
 			for (int y = 0; y < chessboardSize; y++) {
 				final int x = diagonal - chessboardSize + 1 + y;
-				testsCount++;
-				if (x >= 0 && x < chessboardSize && chessboard.get(x).get(y).booleanValue()) {
-					if (usedDiagonal) {
-						return false;
+				explicitTestsCount++;
+				if (x >= 0) {
+					explicitTestsCount++;
+					if (x < chessboardSize) {
+						explicitTestsCount++;
+						methodCallsCount += 3;
+						squareReadsCount++;
+						if (chessboard.get(x).get(y).booleanValue()) {
+
+							explicitTestsCount++;
+							if (usedDiagonal) {
+								return false;
+							}
+							usedDiagonal = true;
+						}
 					}
-					usedDiagonal = true;
 				}
+
+				implicitTestsCount++;
 			}
+
+			implicitTestsCount++;
 		}
 
 		return true;
